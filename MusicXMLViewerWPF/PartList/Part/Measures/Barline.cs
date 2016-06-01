e55c4e363b6b99db4d6d2ca70ebc48cc660d7cde
krzychu124 <krzychu124@gmail.com>
2016-06-01 02:52:05 +0200

    Rework attempt to Measure, completing necessary methods in Barline

--- MusicXMLViewerWPF/PartList/Part/Measures/Barline.cs
+++ MusicXMLViewerWPF/PartList/Part/Measures/Barline.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,27 +8,71 @@ using System.Xml.Linq;
 
 namespace MusicXMLViewerWPF
 {
-    class Barline : Measures, IDrawable
+    class Barline :  IDrawable, IXMLExtract // TODO test class
     {
         
         private BarlineLocation location;
         private BarStyle style;
         private bool coda;
         private bool fermata;
-        private bool segno;
+        private Segno segno;
         private MeasureEnding ending;
+        private Repeat repeat;
+        private Winged winged;
 
         public BarlineLocation Location {  get { return location; } } 
         public BarStyle Style { get { return style; } }
         public bool IsCoda { get { return coda; } }
         public bool IsFermata { get { return fermata; } }
-        public bool IsSegno { get { return segno; } }
+        public Segno Segno{ get { return segno; } }
         public MeasureEnding Ending { get { return ending; } }
+        public Repeat Repeat { get { return repeat; } }
+        public Winged Winged { get { return winged; } }
 
+        public Barline(XElement x)
+        {
+            XMLFiller(x);
+        }
+
+        public void XMLFiller(XElement x)
+        {
+            location = x.Attribute("location").Value == "left" ? BarlineLocation.left : x.Attribute("location").Value == "middle" ? BarlineLocation.middle : BarlineLocation.right;
+            if (x.Element("bar-style") != null)
+            {
+                getStyle(x.Element("bar-style").Value);
+            }
+            if (x.Element("segno") != null)
+            {
+                var attr = from at in x.Element("segno").Attributes() select at;
+                segno = new Segno(attr);
+            }
+        }
         public void Draw(CanvasList surface)
         {
-
+            
         }
+
+        private void getStyle(string s)
+        {
+            style = BarStyle.regular;
+            if (styleDictionary.ContainsKey(s))
+            {
+                style = styleDictionary[s];
+            }
+        }
+        private Dictionary<string, BarStyle> styleDictionary = new Dictionary<string, BarStyle>()
+        {
+            {"regular",BarStyle.regular },
+            {"dotted",BarStyle.dotted },
+            {"dashed",BarStyle.dashed },
+            {"heavy",BarStyle.heavy },
+            {"light-light",BarStyle.light_light },
+            {"light-heavy",BarStyle.light_heavy },
+            {"heavy-light",BarStyle.heavy_light },
+            {"heavy-heavy",BarStyle.heavy_heavy },
+            {"tick",BarStyle.tick },
+            {"short",BarStyle.shortened },
+        };
 
         internal enum BarStyle
         {
@@ -51,8 +96,16 @@ namespace MusicXMLViewerWPF
         }
 
     }
-
-    class MeasureEnding : Barline, IDrawable
+    class Segno : EmptyPrintStyle
+    {
+        private string name = "segno";
+        public string Name { get { return name; } }
+        public Segno(IEnumerable<XAttribute> x) : base(x)
+        {
+            
+        }
+    }
+    class MeasureEnding : IDrawable
     {
         private EndingType type;
         private float end_length;
@@ -96,7 +149,7 @@ namespace MusicXMLViewerWPF
     }
     
     
-    class Repeat : Barline, IDrawable
+    class Repeat : IDrawable
     {
         private int times;
         private RepeatDirection direction;
@@ -117,7 +170,7 @@ namespace MusicXMLViewerWPF
             forward
         }
     }
-    class Winged : Barline, IDrawable, IXMLExtract
+    class Winged :  IDrawable, IXMLExtract
     {
         private WingType type;
         private string s_type;
@@ -184,5 +237,70 @@ namespace MusicXMLViewerWPF
             double_straight,
             double_curved
         }
+    }
+    class EmptyPrintStyle // TODO test class
+    {
+        float def_x;
+        float def_y;
+        float rel_x;
+        float rel_y;
+        Halign h_align;
+        Valign v_align;
+        string color;
+        public EmptyPrintStyle()
+        {
+
+        }
+        public EmptyPrintStyle(IEnumerable<XAttribute> x)
+        {
+            FillAttributes(x);
+        }
+        private void FillAttributes(IEnumerable<XAttribute> x) // TODO check if it's working
+        {
+            foreach (var item in x)
+            {
+                string name = item.Name.LocalName;
+                switch (name)
+                {
+                    case "default-x":
+                        def_x = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "default-y":
+                        def_y = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "relative-x":
+                        rel_x = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "relative-y":
+                        rel_y = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "valign":
+                        string vval = item.Value;
+                        v_align = vval == "top" ? Valign.top : vval == "bottom" ? Valign.bottom : vval == "middle" ? Valign.middle : Valign.baseline;
+                        break;
+                    case "halign":
+                        string hval = item.Value;
+                        h_align = hval == "left" ? Halign.left : hval == "center" ? Halign.center : Halign.right;
+                        break;
+                    case "color":
+                        color = item.Value;
+                        break;
+
+                }
+            }
+        }
+    }
+    enum Halign
+    {
+        left,
+        center,
+        right
+    }
+    enum Valign
+    {
+        top,
+        middle,
+        bottom,
+        baseline
     }
 }
---
+++ MusicXMLViewerWPF/PartList/Part/Measures/Measure.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MusicXMLViewerWPF.PartList.Part.Measures
+{
+    class Measure : IXMLExtract, IDrawable // UNDONE finish reworking class
+    {
+        private int number;
+        private float width;
+        private bool hasNumberVisible;
+
+        public List<Note> NotesList = new List<Note>(); // experimental
+        public Barline Barline;
+        public Print PrintProperties;
+        public Directions Direction;
+        public Attributes Attributes;
+
+        public int Number { get { return number; } }
+        public float Width { get { return width; } }
+        public bool NumberVisible { get { return hasNumberVisible; } }
+
+        public Measure(XElement x)
+        {
+            XMLFiller(x);
+        }
+        public IEnumerable<XElement> XMLExtractor()
+        {
+            var x = Misc.LoadFile.Document;
+            var extracted = from extr in x.Descendants("measure") select extr;
+            return extracted;
+        }
+        public void XMLFiller(XElement x)
+        {
+            width = float.Parse(x.Attribute("width").Value, CultureInfo.InvariantCulture);
+            number = Convert.ToInt32(x.Attribute("number").Value);
+
+        }
+        public void Draw(CanvasList surface)
+        {
+
+        }
+    }
+}
