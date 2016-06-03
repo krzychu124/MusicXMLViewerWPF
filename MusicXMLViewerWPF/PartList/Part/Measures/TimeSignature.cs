e3556abcb9ae21b60098e4df70d2b74a157c4a89
krzychu124 <krzychu124@gmail.com>
2016-06-03 21:33:38 +0200

    implemented ctor, not tested

--- MusicXMLViewerWPF/PartList/Part/Measures/TimeSignature.cs
+++ MusicXMLViewerWPF/PartList/Part/Measures/TimeSignature.cs
@@ -7,68 +7,92 @@ using System.Xml.Linq;
 
 namespace MusicXMLViewerWPF
 {
-    public class TimeSignature : MusicalChars //TODO implement Xelement ctor
+    public class TimeSignature : EmptyPrintStyle // MusicalChars // TODO_L test timesig class
     {
-        private SignatureType sigType;
         private int beats;
         private int beats_type;
         private int measure_num;
+        private int number; // staff number
+        private SignatureType sigType;
         private string beats_str;
         private string beats_type_str;
 
-        public TimeSignature(XElement x)
-        {
 
+        public int Beats { get { return beats; } }
+        public int BeatsType { get { return beats_type; } }
+        public int MeasureNum { get { return measure_num; } }
+        public string BeatStr { get { return beats_str; } }
+        public string BeatTypeStr { get { return beats_type_str; } }
+        //TODO_L implement missing properties, separator,interchangeable
+        public TimeSignature(XElement x) : base (x.Attributes())
+        {
+            var ele = x.Elements();
+            foreach (var item in ele)
+            {
+                string name = item.Name.LocalName;
+                switch (name)
+                {
+                    case "beats":
+                        beats = int.Parse(item.Value);
+                        break;
+                    case "beats-type":
+                        beats_type = int.Parse(item.Value);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            var attr = x.Attributes();
+            foreach (var item in attr) // with switch,dictionary would look better, left it for now
+            {
+                if(item.Name.LocalName == "symbol")
+                {
+                    SetTimeType(item.Value);
+                }
+                if(item.Name.LocalName == "number")
+                {
+                    number = int.Parse(item.Value);
+                }
+            }
+            SetBeatTime(beats_type);
+            SetBeat(beats);
         }
 
         public TimeSignature(int beats, int beats_type,string symbol, int num)
         {
 
-            switch (symbol)
-            {
-                case "common": this.sigType = SignatureType.common;
-                    break;
-                case "cut": this.sigType = SignatureType.cut;
-                    break;
-                default: sigType = SignatureType.number;
-                    break;
-            }
+            SetTimeType(symbol);
             this.beats = beats;
             this.beats_type = beats_type;
-            this.type = MusSymbolType.TimeSignature;
+           // this.type = MusSymbolType.TimeSignature; //changed derived class
             this.measure_num = num;
-            setBeatTime(beats_type);
-            setBeat(beats);
+            SetBeatTime(beats_type);
+            SetBeat(beats);
         }
-        
-        public int Beats
+        private void SetTimeType(string s)
         {
-            get { return beats; }
+            switch (s)
+            {
+                case "common":
+                    this.sigType = SignatureType.common;
+                    break;
+                case "cut":
+                    this.sigType = SignatureType.cut;
+                    break;
+                default:
+                    sigType = SignatureType.number;
+                    break;
+            }
+
         }
-        public int BeatsType
-        {
-            get { return beats_type; }
-        }
-        public int MeasureNum
-        {
-            get { return measure_num; }
-        }
-        public string BeatStr
-        {
-            get { return beats_str; }
-        }
-        public string BeatTypeStr
-        {
-            get { return beats_type_str; }
-        }
-        private void setBeatTime(int i)
+        private void SetBeatTime(int i)
         {
             if (beat_dic.ContainsKey(i))
             {
                 this.beats_type_str = beat_dic[i];
             }
         }
-        private void setBeat(int i)
+        private void SetBeat(int i)
         {
             if (beat_d.ContainsKey(i))
             {
