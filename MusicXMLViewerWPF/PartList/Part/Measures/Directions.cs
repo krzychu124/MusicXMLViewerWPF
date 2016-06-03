eb208807c2796d2d3138e677644bd58b177346e2
krzychu124 <krzychu124@gmail.com>
2016-06-03 23:38:16 +0200

    implemented missing elements, compatibility changes

--- MusicXMLViewerWPF/PartList/Part/Measures/Directions.cs
+++ MusicXMLViewerWPF/PartList/Part/Measures/Directions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,41 +8,61 @@ using System.Xml.Linq;
 
 namespace MusicXMLViewerWPF
 {
-    class Directions : MusicalChars
+    public class Direction : MusicalChars
     {
-        //attributes//
+        #region Attributes
         private string placement;
         private string directive;
-        //additional settings//
-        private Dynamics dynamics;
+        #endregion
+        #region Elements
+        private Dynamics dynamics; // TODO_L will be moved to directions;
+        private List<Directions> directionList = new List<Directions>();
         private DirectionType typ; // TODO_H rework to class, then add all attributes
-        private int posY;
-        private int relX;
+        private int posY; // may be not usable, during rework...
+        private int relX; // may be not usable, during rework...
         private float offset;
         private int staff;
-
-        public Directions(XElement x)
+        #endregion
+        public Direction(XElement x)
         {
             var directions = x.Element("direction");
             placement = null;
             directive = null;
-            if (directions.HasAttributes)
+            if (directions.HasAttributes) // seems to be done for now
             {
                 placement = directions.Attribute("placement") != null ? directions.Attribute("placement").Value : null;
                 directive = directions.Attribute("directive") != null ? directions.Attribute("directive").Value : null;
             }
             if (directions.HasElements)
             {
-
+                var elements = x.Elements();
+                foreach (var item in elements)
+                {
+                    switch (name)
+                    {
+                        case "staff":
+                            staff = int.Parse(item.Value);
+                            break;
+                        case "direction-type":
+                            var direction = new Directions(item);
+                            directionList.Add(direction);
+                            break;
+                        case "offset":
+                            offset = float.Parse(item.Value);
+                            break; 
+                        default: Logger.Log("not implemented switch element");
+                            break;
+                    }
+                }
             }
         }
-        public Directions(Dynamics dynamic, string placement)
+        public Direction(Dynamics dynamic, string placement)
         {
             typ = DirectionType.dynamics;
             dynamics = dynamic;
             this.placement = placement;
         }
-        public Directions(string placement, string direct_type, int posY, string type)
+        public Direction(string placement, string direct_type, int posY, string type)
         {
             this.placement= placement;
             this.posY = posY;
@@ -56,11 +77,187 @@ namespace MusicXMLViewerWPF
             }
         }
     }
-    enum DirectionType
+
+    public class Directions
+    {
+        private Wedge wedge = null;
+        private Segno segno = null;
+        private Rehearsal rehearsal = null;
+        private Coda coda = null;
+        private Dynamics dynamics = null;
+        private OtherDirection other = null;
+
+        public Wedge Wedge { get { return wedge; } }
+        public Segno Segno { get { return segno; } }
+        public Rehearsal Rehearsal { get { return rehearsal; } }
+        public Coda Coda { get { return coda; } }
+        public Dynamics Dynamics { get { return dynamics; } }
+        public OtherDirection Other { get { return other; } }
+
+        public Directions(XElement x)
+        {
+            string name = x.Name.LocalName;
+            switch (name)
+            {
+                case "wedge":
+                    wedge = new Wedge(x);
+                    break;
+                case "rehearsal":
+                    rehearsal = new Rehearsal(x);
+                    break;
+                case "segno":
+                    segno = new Segno(x.Attributes());
+                    break;
+                case "coda":
+                    coda = new Coda(x.Attributes());
+                    break;
+                case "dymnamics":
+                    dynamics = new Dynamics(x);
+                    break;
+                case "other-direction":
+                    other = new OtherDirection(x);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+    }
+
+    public class OtherDirection : EmptyPrintStyle, IDirections
+    {
+        private string id;
+        private string value;
+
+        public string Id { get { return id; } }
+        public string Value { get { return value; } }
+
+        public OtherDirection(XElement x) : base (x.Attributes())
+        {
+            value =  x.Value;
+        }
+    }
+
+    enum DirectionType //TODO_L may be not usable due to rework whole class
     {
         other,
         wedge,
         dynamics,
        // words, not now
+    }
+    public class Wedge : PositionHelper, IDirections
+    {
+        private string id= "wedge";
+        private WedgeType type;
+        private int number;
+        private float spread;
+        private float dash_length;
+        private float space_length;
+
+        public string Id { get { return id; } }
+        public WedgeType Type { get { return type; } }
+        public int Number { get { return number; } }
+        public float Spread { get { return spread; } }
+        public float DLength { get { return dash_length;  } }
+        public float SLength { get { return space_length; } }
+
+        public Wedge(XElement x) : base (x.Attributes())
+        {
+            var attr = x.Attributes();
+            foreach (var item in attr)
+            {
+                string name = item.Name.LocalName;
+                switch (name)
+                {
+                    case "type":
+                        type = item.Value == "crescendo" ? WedgeType.crescendo : item.Value == "diminuendo" ? WedgeType.diminuendo : item.Value == "stop" ? WedgeType.stop : WedgeType.next;
+                        break;
+                    case "number":
+                        number = int.Parse(item.Value);
+                        break;
+                    case "spread":
+                        spread = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "dash-length":
+                        dash_length = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "space-length":
+                        space_length = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public enum WedgeType
+        {
+            crescendo,
+            diminuendo,
+            stop,
+            next
+        }
+        
+    }
+    public class Rehearsal : EmptyPrintStyle, IDirections
+    {
+        private string id ="rehearsal";
+        private string value;
+
+        public string Id { get { return id; } }
+        public string Value { get { return value; } }
+
+        public Rehearsal(XElement x) : base (x.Attributes())
+        {
+            value = x.Value;
+        }
+
+    }
+    public interface IDirections
+    {
+        string Id { get; }
+    }
+
+    public enum LineType
+    {
+        solid,
+        dashed,
+        dotted,
+        wavy
+    }
+
+    public class PositionHelper
+    {
+        private float def_x;
+        private float def_y;
+        private float rel_x;
+        private float rel_y;
+
+        public float DefX { get { return def_x; } }
+        public float DefY { get { return def_y; } }
+        public float RelX { get { return rel_x; } }
+        public float RelY { get { return rel_y; } }
+
+        public PositionHelper(IEnumerable<XAttribute> x) // TODO_L check if it's working
+        {
+            foreach (var item in x)
+            {
+                string name = item.Name.LocalName;
+                switch (name)
+                {
+                    case "default-x":
+                        def_x = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "default-y":
+                        def_y = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "relative-x":
+                        rel_x = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                    case "relative-y":
+                        rel_y = float.Parse(item.Value, CultureInfo.InvariantCulture);
+                        break;
+                }
+            }
+        }
     }
 }
--- MusicXMLViewerWPF/PartList/Part/Measures/Dynamics.cs
+++ MusicXMLViewerWPF/PartList/Part/Measures/Dynamics.cs
@@ -3,34 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MusicXMLViewerWPF
 {
-    class Dynamics // need rework
+    public class Dynamics : EmptyPrintStyle// need rework
     {
-        public int posX;
-        public int posY;
-        public string placement;
-        public int halign;
-        public DynamicType type;
+        //public int posX;
+       // public int posY;
+        private string other;
+        private string placement;
+        //public int halign;
+        private DynamicType type;
+        
         public bool isRelative;
-        public Dynamics()
+        public Dynamics(XElement x) : base (x.Attributes())
         {
-
+            placement = x.Attribute("placement") != null ?  x.Attribute("placement").Value : "below";
+            SetDynType(x.Value);
         }
-        public Dynamics( int y, int align, string t, int x=0)
+        public Dynamics( int y, string align, string t, int x=0)//TODO_L improve, currently temp, only for compatibility while reworking
         {
             isRelative = x != 0 ? true : false;
-            this.posX = x;
-            this.posY = y;
-            halign = align;
-            setDynType(t);
+            def_x = x;
+            def_y = y;
+            h_align = align == "left" ? Halign.left : align == "center" ? Halign.center : Halign.right; 
+            SetDynType(t);
         }
-        public void setDynType(string t)
+        public void SetDynType(string t)
         {
             if (dynType_dict.ContainsKey(t))
             {
+                other = null;
                 type = dynType_dict[t];
+            }
+            else
+            {
+                type = DynamicType.other;
+                other = t;
             }
         }
         public Dictionary<string, DynamicType> dynType_dict = new Dictionary<string, DynamicType> {
