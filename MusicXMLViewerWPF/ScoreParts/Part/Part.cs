fb0d024a205c9c1f49270e690f652061fb4368ad
krzychu124 <krzychu124@gmail.com>
2016-06-05 02:34:15 +0200

    added few files, implemented basic elements to new Part class, not tested yet

---
+++ MusicXMLViewerWPF/ScoreParts/Part/Part.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MusicXMLViewerWPF.ScoreParts.Part
+{
+    class Part
+    {
+        private string part_id;
+        private List<Measures.Measure> measure_list = new List<Measures.Measure>();
+
+        public string Id { get { return part_id; } }
+        public List<Measures.Measure> MeasureList { get { return measure_list; } }
+       
+
+        public Part(XElement x)
+        {
+            part_id = x.Attribute("id").Value;
+            var measures = x.Elements();
+            foreach (var item in measures)
+            {
+                measure_list.Add(new Measures.Measure(item));
+            }
+        }
+    }
+}
