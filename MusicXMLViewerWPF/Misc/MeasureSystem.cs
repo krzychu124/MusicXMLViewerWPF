284021a9b85e0a7c2117db4820469e347a431fb5
krzychu124 <krzychu124@gmail.com>
2016-08-12 02:47:10 +0200

    class containing list of measure systems (lines of measures)

---
+++ MusicXMLViewerWPF/Misc/MeasureSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicXMLViewerWPF.Misc
+{
+    class MeasureSystem
+    {
+        /// <summary>
+        /// List of measures in system(eg. measures placed in one line)
+        /// </summary>
+        private static List<ScoreParts.Part.Measures.Measure> newsystem = new List<ScoreParts.Part.Measures.Measure>();
+
+    }
+}
