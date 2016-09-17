f045a2912664cf6c770a2e5783a5f7a13ccc44cb
krzychu124 <krzychu124@gmail.com>
2016-09-17 23:04:53 +0200

    Random Unique ID generator

---
+++ MusicXMLViewerWPF/Misc/RandomGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicXMLViewerWPF.Misc
+{
+    class RandomGenerator
+    {
+        static Random random = new Random();
+        public static string GetRandomHexNumber(int digits = 8)
+        {
+            byte[] buffer = new byte[digits / 2];
+            random.NextBytes(buffer);
+            string result = String.Concat(buffer.Select(x => x.ToString("X2")).ToArray());
+            if (digits % 2 == 0)
+                return result;
+            return result + random.Next(16).ToString("X");
+        }
+    }
+}
