a144f68eca1df1774e19643faa196357ff61e0cd
krzychu124 <krzychu124@gmail.com>
2016-10-09 20:52:06 +0200

    +GetSegment using id

--- MusicXMLViewerWPF/Misc/ScoreSystem.cs
+++ MusicXMLViewerWPF/Misc/ScoreSystem.cs
@@ -12,6 +12,23 @@ namespace MusicXMLViewerWPF.Misc
         /// List of segments
         /// </summary>
         public static Dictionary<string, Segment> Segments = new Dictionary<string, Segment>();
+        public static void Clear()
+        {
+            Segments.Clear();
+        }
+        public static Segment GetSegment(string id)
+        {
+            Segment segment = new Segment();
+            if (Segments.ContainsKey(id))
+            {
+                segment = Segments[id];
+            }
+            else
+            {
+                Console.WriteLine($"Segment list NOT contain folowing key {id}");
+            }
+            return segment;
+        }
         //static float test = 0;
         //public static void s (int i)
         //{
