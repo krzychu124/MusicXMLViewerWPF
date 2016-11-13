183248e1efd72d0ba5e620500611325f6c91f308
krzychu124 <krzychu124@gmail.com>
2016-11-13 02:40:55 +0100

    tree view of loaded xml - few improvements needed

--- MusicXMLViewerWPF/Log/LogViewModel.cs
+++ MusicXMLViewerWPF/Log/LogViewModel.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using MusicXMLScore.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace MusicXMLScore.Log
 {
-    class LogViewModel
+    class LogViewModel : INotifyPropertyChanged
     {
         private static LoggIt log;
         private ObservableCollection<LoggIt.Logger> infolog;
@@ -15,15 +18,20 @@ namespace MusicXMLScore.Log
         private ObservableCollection<LoggIt.Logger> errorlog;
         private ObservableCollection<LoggIt.Logger> exceptionslog;
         private ObservableCollection<LoggIt.Logger> alllog;
+        private XmlDataProvider xmlfile;
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
         public ObservableCollection<LoggIt.Logger> InfoLog { get { return infolog; }  set { infolog = value; } }
         public ObservableCollection<LoggIt.Logger> WarningLog { get { return warninglog; } set { warninglog = value; } }
         public ObservableCollection<LoggIt.Logger> ErrorLog { get { return errorlog; } set { errorlog = value; } }
         public ObservableCollection<LoggIt.Logger> ExceptionsLog { get { return exceptionslog; } set { exceptionslog = value; } }
         public ObservableCollection<LoggIt.Logger> AllLog { get { return alllog; } set { alllog = value; } }
+        public XmlDataProvider XMLLoadedFile { get { return xmlfile; } set { xmlfile = value; PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(XMLLoadedFile))); } }
 
         public LogViewModel()
         {
+            Mediator.Register("XmlFileLoaded", SetXmlFile);
             log = new LoggIt();
             infolog = LoggIt.Infolog;
             warninglog = LoggIt.Warninglog;
@@ -32,6 +40,11 @@ namespace MusicXMLScore.Log
             alllog = LoggIt.Alllog;
             LoggIt.Log("test1");
             LoggIt.Log("test2", LogType.Warning);
+        }
+
+        private void SetXmlFile(object xml)
+        {
+            XMLLoadedFile = xml as XmlDataProvider;
         }
     }
 }
