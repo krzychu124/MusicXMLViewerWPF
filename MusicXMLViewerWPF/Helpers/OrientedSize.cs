13a4b99c56af2d13ed822049d7d468b8e2eddd1c
krzychu124 <krzychu124@gmail.com>
2016-11-14 20:26:59 +0100

    orented size for oriented items calculation

---
+++ MusicXMLViewerWPF/Helpers/OrientedSize.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MusicXMLScore.Helpers
+{
+    class OrientedSize
+    {
+        private Orientation orientation;
+        private double direct;
+        private double indirect;
+
+        public double Direct { get { return direct; } set { direct = value; } }
+        public double Indirect { get { return indirect; } set { indirect = value; } }
+        public Orientation Orientation { get { return orientation; } }
+        public double Width
+        {
+            get
+            {
+                return (Orientation == Orientation.Horizontal) ? direct : indirect;
+            }
+            set
+            {
+                if (Orientation == Orientation.Horizontal)
+                {
+                    Direct = value;
+                }
+                else
+                {
+                    Indirect = value;
+                }
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return (Orientation != Orientation.Horizontal) ? direct : indirect;
+            }
+            set
+            {
+                if (Orientation != Orientation.Horizontal)
+                {
+                    Direct = value;
+                }
+                else
+                {
+                    Indirect = value;
+                }
+            }
+        }
+
+        public OrientedSize(Orientation orientation, double width, double height)
+        {
+            this.orientation = orientation;
+            direct = 0.0;
+            indirect = 0.0;
+            Width = width;
+            Height = height;
+        }
+
+        public OrientedSize(Orientation orientation):this(orientation, 0.0, 0.0)
+        {
+
+        }
+    }
+}
