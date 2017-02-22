effffafedfc7420c0a953fe6b316586078af76ab
krzychu124 <krzychu124@gmail.com>
2017-02-22 21:52:02 +0100

    forward _time - serialization

--- MusicXMLViewerWPF/Model/MeasureItems/ForwardMusicXML.cs
+++ MusicXMLViewerWPF/Model/MeasureItems/ForwardMusicXML.cs
@@ -1,6 +1,61 @@
-﻿namespace MusicXMLScore.Model.MeasureItems
+﻿using System;
+using System.Xml.Serialization;
+
+namespace MusicXMLScore.Model.MeasureItems
 {
+    [Serializable]
+    [XmlType(TypeName = "forward")]
     public class ForwardMusicXML
     {
+        private double duration;
+        //formattedTest footnote
+        //level levelField;
+        private string voice;
+        private string staff;
+
+        public ForwardMusicXML()
+        {
+
+        }
+
+        [XmlElement("duration")]
+        public double Duration
+        {
+            get
+            {
+                return duration;
+            }
+
+            set
+            {
+                duration = value;
+            }
+        }
+        [XmlElement("voice")]
+        public string Voice
+        {
+            get
+            {
+                return voice;
+            }
+
+            set
+            {
+                voice = value;
+            }
+        }
+        [XmlElement("staff", DataType ="positiveInteger")]
+        public string Staff
+        {
+            get
+            {
+                return staff;
+            }
+
+            set
+            {
+                staff = value;
+            }
+        }
     }
 }
