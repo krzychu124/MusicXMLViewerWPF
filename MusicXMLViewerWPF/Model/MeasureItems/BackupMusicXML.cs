e73554d8becdc4f04bfb2f681512ea8da6cf3c79
krzychu124 <krzychu124@gmail.com>
2017-02-22 21:51:45 +0100

    backup _time - serialization

--- MusicXMLViewerWPF/Model/MeasureItems/BackupMusicXML.cs
+++ MusicXMLViewerWPF/Model/MeasureItems/BackupMusicXML.cs
@@ -1,6 +1,32 @@
-﻿namespace MusicXMLScore.Model.MeasureItems
+﻿using System;
+using System.Xml.Serialization;
+
+namespace MusicXMLScore.Model.MeasureItems
 {
+    [Serializable]
+    [XmlType("backup")]
     public class BackupMusicXML
     {
+        private double duration;
+        //formattedText footnote
+        //level levelField;
+
+        public BackupMusicXML()
+        {
+
+        }
+
+        [XmlElement("duration")]
+        public double Duration
+        {
+            get
+            {
+                return duration;
+            }
+            set
+            {
+                duration = value;
+            }
+        }
     }
 }
