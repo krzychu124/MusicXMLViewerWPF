1025c80cccb98274dd598cb2ba0a4bb69860640e
krzychu124 <krzychu124@gmail.com>
2017-02-26 03:38:28 +0100

    ++placement empty classes, ++accidental mark - serialization ++ articulations - serialization

---
+++ MusicXMLViewerWPF/Model/Helpers/EmptyLineMusicXML.cs
@@ -0,0 +1,162 @@
+﻿using MusicXMLScore.Model.Helpers.SimpleTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MusicXMLScore.Model.Helpers
+{
+    [Serializable]
+    [XmlType(TypeName ="empty-line")]
+    public class EmptyLineMusicXML
+    {
+        private LineShapeMusicXML lineshape;
+        private bool lineshapeSpecified;
+        private LineTypeMusicXML linetype;
+        private bool linetypeSpecified;
+        private double dashlength;
+        private bool dashlengthSpecified;
+        private double spacelength;
+        private bool spacelengthSpecified;
+        private AboveBelowMusicXML placement;
+        private bool placementSpecified;
+
+        public EmptyLineMusicXML()
+        {
+
+        }
+
+        [XmlAttribute("line-shape")]
+        public LineShapeMusicXML Lineshape
+        {
+            get
+            {
+                return lineshape;
+            }
+
+            set
+            {
+                lineshape = value;
+            }
+        }
+        [XmlIgnore]
+        public bool LineshapeSpecified
+        {
+            get
+            {
+                return lineshapeSpecified;
+            }
+
+            set
+            {
+                lineshapeSpecified = value;
+            }
+        }
+        [XmlAttribute("line-type")]
+        public LineTypeMusicXML Linetype
+        {
+            get
+            {
+                return linetype;
+            }
+
+            set
+            {
+                linetype = value;
+            }
+        }
+        [XmlIgnore]
+        public bool LinetypeSpecified
+        {
+            get
+            {
+                return linetypeSpecified;
+            }
+
+            set
+            {
+                linetypeSpecified = value;
+            }
+        }
+        [XmlAttribute("dash-length")]
+        public double Dashlength
+        {
+            get
+            {
+                return dashlength;
+            }
+
+            set
+            {
+                dashlength = value;
+            }
+        }
+        [XmlIgnore]
+        public bool DashlengthSpecified
+        {
+            get
+            {
+                return dashlengthSpecified;
+            }
+
+            set
+            {
+                dashlengthSpecified = value;
+            }
+        }
+        [XmlAttribute("space-length")]
+        public double Spacelength
+        {
+            get
+            {
+                return spacelength;
+            }
+
+            set
+            {
+                spacelength = value;
+            }
+        }
+        [XmlIgnore]
+        public bool SpacelengthSpecified
+        {
+            get
+            {
+                return spacelengthSpecified;
+            }
+
+            set
+            {
+                spacelengthSpecified = value;
+            }
+        }
+        [XmlAttribute("placement")]
+        public AboveBelowMusicXML Placement
+        {
+            get
+            {
+                return placement;
+            }
+
+            set
+            {
+                placement = value;
+            }
+        }
+        [XmlIgnore]
+        public bool PlacementSpecified
+        {
+            get
+            {
+                return placementSpecified;
+            }
+
+            set
+            {
+                placementSpecified = value;
+            }
+        }
+    }
+}
---
+++ MusicXMLViewerWPF/Model/Helpers/PlacementTextMusicXML.cs
@@ -0,0 +1,59 @@
+﻿using MusicXMLScore.Model.Helpers.SimpleTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MusicXMLScore.Model.Helpers
+{
+    [Serializable]
+    [XmlType(TypeName ="placement-text")]
+    public class PlacementTextMusicXML
+    {
+        private AboveBelowMusicXML placement;
+        private bool placementSpecified;
+        private string value;
+
+        [XmlAttribute("placement")]
+        public AboveBelowMusicXML Placement
+        {
+            get
+            {
+                return placement;
+            }
+
+            set
+            {
+                placement = value;
+            }
+        }
+        [XmlIgnore]
+        public bool PlacementSpecified
+        {
+            get
+            {
+                return placementSpecified;
+            }
+
+            set
+            {
+                placementSpecified = value;
+            }
+        }
+        [XmlText]
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+    }
+}
