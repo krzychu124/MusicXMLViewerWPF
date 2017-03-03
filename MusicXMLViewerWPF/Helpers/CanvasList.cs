22d86997a452d8eeac1c2712941ad4cbaebb42ac
krzychu124 <krzychu124@gmail.com>
2017-03-03 03:34:46 +0100

    tooltip for debugging

--- MusicXMLViewerWPF/Helpers/CanvasList.cs
+++ MusicXMLViewerWPF/Helpers/CanvasList.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace MusicXMLScore.Helpers
 {
     class CanvasList : Panel
     {
+        ToolTip t = new ToolTip();
         //private VisualCollection visuals;
         private List<Visual> visuals = new List<Visual>();
         public List<Visual> Visuals { get { return visuals; } }
@@ -27,6 +29,61 @@ namespace MusicXMLScore.Helpers
         {
             this.Width = width;
             this.Height = height;
+            this.MouseEnter += new MouseEventHandler(MyVisualHost_MouseEnter);
+            this.MouseLeave += new MouseEventHandler(MyVisualHost_MouseLeave);
+        }
+
+        private void MyVisualHost_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (t.IsOpen == true)
+                t.IsOpen = false;
+        }
+
+        private void MyVisualHost_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Point pt = e.GetPosition((UIElement)sender);
+            EllipseGeometry expandedHitTestArea = new EllipseGeometry(pt, 10.0, 10.0);
+            VisualTreeHelper.HitTest(this, null, new HitTestResultCallback(myCallback), new GeometryHitTestParameters(expandedHitTestArea));
+        }
+
+        private HitTestResultBehavior myCallback(HitTestResult result) //TODO more hit tests later
+        {
+            IntersectionDetail intersectionDetail = ((GeometryHitTestResult)result).IntersectionDetail;
+            switch (intersectionDetail)
+            {
+                case IntersectionDetail.NotCalculated:
+                    return HitTestResultBehavior.Continue;
+                case IntersectionDetail.Empty:
+                    return HitTestResultBehavior.Continue;
+                case IntersectionDetail.FullyInside:
+                    bool stop = OpenToolTip(result);
+                    return stop ? HitTestResultBehavior.Stop : HitTestResultBehavior.Continue;
+                case IntersectionDetail.FullyContains:
+                    bool stop2 = OpenToolTip(result);
+                    return stop2 ? HitTestResultBehavior.Stop : HitTestResultBehavior.Continue;
+                case IntersectionDetail.Intersects:
+                    bool stop3 = OpenToolTip(result);
+                    return HitTestResultBehavior.Stop;
+                default:
+                    return HitTestResultBehavior.Stop;
+            }
+        }
+        private bool OpenToolTip(HitTestResult result)
+        {
+            if (result.VisualHit.GetType() == typeof(DrawingVisual))
+            {
+                if (!t.IsOpen)
+                {
+                    if (t.Content == null)
+                    {
+                        return false;
+                    }
+                    t.IsOpen = true;
+                    t.PlacementTarget = this;
+                }
+
+            }
+            return true;
         }
         public CanvasList(Size size) : base()
         {
@@ -96,6 +153,16 @@ namespace MusicXMLScore.Helpers
             {
                 DeleteVisual(visuals[0]);
             }
+        }
+        public void SetToolTipText(string text)
+        {
+            ToolTip tip = new ToolTip();
+            tip.Content = text;
+            tip.InvalidateVisual();
+            //this.ToolTip = tip;
+            this.t = tip;
+            //t.Width = Double.NaN;
+            //t.Height = Double.NaN;
         }
         public int Count { get { return visuals.Count; } }
     }
