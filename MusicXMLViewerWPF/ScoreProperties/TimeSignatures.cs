7e190defa9fc539c0a170bdd6d9d07a545ba3112
krzychu124 <krzychu124@gmail.com>
2017-03-14 03:06:59 +0100

    timeMusicXML object.Clone(), ++ timeSignatures attributes generated in scorePropertiesContainer to provide access to timeSignature from any measure object

---
+++ MusicXMLViewerWPF/ScoreProperties/TimeSignatures.cs
@@ -0,0 +1,58 @@
+﻿using MusicXMLScore.Model.MeasureItems;
+using MusicXMLScore.Model.MeasureItems.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicXMLScore.ScoreProperties
+{
+    public class TimeSignatures
+    {
+        Dictionary<string, TimeMusicXML> timeSignatures = new Dictionary<string, TimeMusicXML>();
+        MusicXMLViewerWPF.ScorePartwiseMusicXML score;
+        public TimeSignatures(MusicXMLViewerWPF.ScorePartwiseMusicXML score)
+        {
+            this.score = score;
+            if (score.Part != null)
+            {
+                GenerateTimeSignaturesPerMeasure();
+            }
+        }
+        private void GenerateTimeSignaturesPerMeasure()
+        {
+            var firstPart = score.Part.FirstOrDefault();
+            TimeMusicXML currentTimeSignature = new TimeMusicXML();
+            foreach (var measure in firstPart.Measure)
+            {
+                if (measure.Items.OfType<AttributesMusicXML>().FirstOrDefault() != null)
+                {
+                    var attributes = measure.Items.OfType<AttributesMusicXML>().FirstOrDefault();
+                    if (attributes.Time.Count != 0)
+                    {
+                        var timeSig = attributes.Time.ElementAt(0);
+                        currentTimeSignature = timeSig;
+                        currentTimeSignature.PrintObject = Model.Helpers.SimpleTypes.YesNoMusicXML.yes;
+                        timeSignatures.Add(measure.Number, currentTimeSignature);
+                    }
+                    else
+                    {
+                        TimeMusicXML time = currentTimeSignature.Clone();
+                        time.PrintObject = Model.Helpers.SimpleTypes.YesNoMusicXML.no;
+                        timeSignatures.Add(measure.Number, time);
+                    }
+                }
+                else
+                {
+                    TimeMusicXML time = currentTimeSignature.Clone();
+                    time.PrintObject = Model.Helpers.SimpleTypes.YesNoMusicXML.no;
+                    timeSignatures.Add(measure.Number, time);
+                }
+              
+            }
+            var test = timeSignatures.Select(i => i).Where(i => i.Value.PrintObject == Model.Helpers.SimpleTypes.YesNoMusicXML.yes);
+        }
+        
+    }
+}
