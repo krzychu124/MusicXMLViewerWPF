da25c12151ab6538699864c378198aba931573a8
krzychu124 <krzychu124@gmail.com>
2017-04-13 04:33:02 +0200

    removed MeasureAttributesContainer class due to previous refactoring

--- MusicXMLViewerWPF/LayoutControl/SegmentPanel.cs
+++ MusicXMLViewerWPF/LayoutControl/SegmentPanel.cs
@@ -14,14 +14,11 @@ namespace MusicXMLScore.LayoutControl
 {
     class SegmentPanel : Panel
     {
-        private Dictionary<int, MeasureAttributesContainer> attributesContainer;
         private Dictionary<int, MeasureItemsContainer> notesContainer;
         private double panelWidth = 0.0;
         private double panelHeight = 0.0;
-        private Dictionary<int, double> stavesLines;
         private int staves = 1;
         private double defaultStavesDistance = 0.0;
-        private LayoutGeneral layoutProperties;
         private string partID;
         private Dictionary<int, double> staffDistances = new Dictionary<int, double>() { { 1, 0.0 } };
         private PartProperties partProperties;
@@ -39,38 +36,14 @@ namespace MusicXMLScore.LayoutControl
             defaultStavesDistance = partProperties.StaffLayoutPerPage.ElementAt(this.pageIndex).ElementAt(systemIndex).StaffDistance;//? StavesDistance;
             SetHeight();
         }
-        public void AddAttributesContainer(MeasureAttributesContainer measureAttributes, int numberOfStave = 1)
-        {
-            measureAttributes.ItemCanvas.Tag = numberOfStave.ToString();
-            Canvas.SetLeft(measureAttributes.ItemCanvas, 0); //adding left offset, refactor to %of staffline height, or add to each child element
-            if (numberOfStave != 1)//works properly if number of staves is 2, higher nuber will overlap with 2nd. stave, WiP
-            {
-                double staffHeight = ViewModel.ViewModelLocator.Instance.Main.CurrentPageLayout.StaffHeight.MMToWPFUnit();
-                Canvas.SetTop(measureAttributes.ItemCanvas, staffHeight + defaultStavesDistance.TenthsToWPFUnit());
-            }
-            Children.Add(measureAttributes.ItemCanvas);
-            InitAttributesContainer();// attributesContainer = new Dictionary<int, MeasureAttributesContainer>();
-            attributesContainer.Add(numberOfStave, measureAttributes);
-        }
         public void AddNotesContainer(MeasureItemsContainer measureNotes, int numberOfStave = 1)
         {
             measureNotes.Tag = numberOfStave.ToString();
-            //if (numberOfStave != 1)
-            //{
-                double staffHeight = ViewModel.ViewModelLocator.Instance.Main.CurrentPageLayout.StaffHeight.MMToWPFUnit();
-                measureNotes.ArrangeStaffs(staffHeight + defaultStavesDistance.TenthsToWPFUnit());
-                //Canvas.SetTop(measureNotes, staffHeight + defaultStavesDistance.TenthsToWPFUnit());
-            //}
+            double staffHeight = ViewModel.ViewModelLocator.Instance.Main.CurrentPageLayout.StaffHeight.MMToWPFUnit();
+            measureNotes.ArrangeStaffs(staffHeight + defaultStavesDistance.TenthsToWPFUnit());
             Children.Add(measureNotes);
             InitNotesContainer();
             notesContainer.Add(numberOfStave, measureNotes);
-        }
-        private void InitAttributesContainer()
-        {
-            if (attributesContainer == null)
-            {
-                attributesContainer = new Dictionary<int, MeasureAttributesContainer>();
-            }
         }
         private void InitNotesContainer()
         {
