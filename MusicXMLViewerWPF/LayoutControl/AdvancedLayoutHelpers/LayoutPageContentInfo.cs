ca569fd5ffea6b9a8d78718e452f75d0f213f9c8
krzychu124 <krzychu124@gmail.com>
2017-05-09 01:07:53 +0200

    layoutSystemInfo, layoutPageContentInfo

---
+++ MusicXMLViewerWPF/LayoutControl/AdvancedLayoutHelpers/LayoutPageContentInfo.cs
@@ -0,0 +1,197 @@
+﻿using MusicXMLScore.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MusicXMLScore.LayoutControl
+{
+    class LayoutPageContentInfo
+    {
+        //! missing custom system distance setter
+        private int pageIndex;
+        private double pageContentWidth;
+        private double pageContentHeight;
+        private bool stretchSystemsToWidth;
+
+        private double defaultStaffDistance;
+        private double defaultSystemDistance;
+        private double defaultTopSystemDistance;
+        Dictionary<int, double> systemDistances;
+        Dictionary<int, double> systemHeights;
+        List<LayoutSystemInfo> systemDimensionsInfo;
+        double availableHeight;
+        List<double> systemsYPositions;
+        List<double> systemsXPositions;
+        public Point SystemPosition(int index) => new Point(systemsXPositions[index], systemsYPositions[index]);
+        public double SystemVerticalPosition(int index) =>  index < systemsYPositions.Count ? systemsYPositions[index] : 0.0;
+        public double SystemHorizontalPosition(int index) => index < systemsXPositions.Count ? systemsXPositions[index] : 0.0;
+        public bool StretchSystemsToWidth
+        {
+            get
+            {
+                return stretchSystemsToWidth;
+            }
+
+            set
+            {
+                stretchSystemsToWidth = value;
+            }
+        }
+
+        public LayoutPageContentInfo(int pageIndex)
+        {
+            this.pageIndex = pageIndex;
+            SetDefaultDistances();
+            GetPageContentHeight();
+            GetPageContentWidth();
+            availableHeight = pageContentHeight; //! CalculateAvailableHeight();
+        }
+        public LayoutPageContentInfo(int pageIndex, double staffDistance, double systemDistance, double topSystemDistance)
+        {
+            this.pageIndex = pageIndex;
+            defaultStaffDistance = staffDistance;
+            defaultSystemDistance = systemDistance;
+            defaultTopSystemDistance = topSystemDistance;
+            GetPageContentHeight();
+            GetPageContentWidth();
+            availableHeight = pageContentHeight; //! CalculateAvailableHeight();
+        }
+        /// <summary>
+        /// Adds Complete Collection of LayoutSystemInfo to LayoutPageContent
+        /// </summary>
+        /// <param name="systemInfo"></param>
+        public void AddSystemDimensionsInfo(List<LayoutSystemInfo> systemInfo)
+        {
+            systemDimensionsInfo = systemInfo;
+            CalculateAvailableHeight();
+        }
+
+        /// <summary>
+        /// Adds LayoutSystemInfo to this LayoutPageContent
+        /// </summary>
+        /// <param name="systemInfo">LayoutSystem instance which represents visual collection of measures arranged into System</param>
+        /// <returns>False if LayoutSystem can't fit on page and should be added to new page</returns>
+        public bool AddSystemDimensionInfo(LayoutSystemInfo systemInfo)
+        {
+            if (systemDimensionsInfo == null)
+            {
+                systemDimensionsInfo = new List<LayoutSystemInfo>();
+            }
+            if (availableHeight < systemInfo.SystemHeight)
+            {
+                return false;
+            }
+            else
+            {
+                //! need tests 
+                //? availableHeight -= systemInfo.SystemHeight + defaultSystemDistance.TenthsToWPFUnit();
+                systemDimensionsInfo.Add(systemInfo);
+                CalculateAvailableHeight();
+                return true;
+            }
+        }
+
+        private void ArrangeSystems()
+        {
+            double currentX = 0.0;
+            double currentY = 0.0;
+            if (systemsXPositions == null || systemsYPositions == null)
+            {
+                systemsXPositions = new List<double>();
+                systemsYPositions = new List<double>();
+            }
+            for (int i = 0; i < systemDimensionsInfo.Count; i++)
+            {
+                systemsXPositions.Add(currentX);
+                systemsYPositions.Add(currentY);
+                currentY += systemDistances[i] + systemHeights[i];
+                currentX = systemDimensionsInfo[i].SystemWidth;
+                if (stretchSystemsToWidth)
+                {
+                    if (currentX < pageContentWidth)
+                    {
+                        //? systemDimensionsInfo[i].UpdateSystemWidth(pageContentWidth);
+                    }
+                }
+            }
+        }
+
+        private void CalculateAvailableHeight()
+        {
+            GenerateSystemHeights();
+            GenerateSystemDistances();
+            for (int i = 0; i < systemDistances.Count; i++)
+            {
+                availableHeight -= systemDistances[i];
+                availableHeight -= systemHeights[i];
+                if (availableHeight < 0)
+                {
+                    availableHeight = 0;
+                    Log.LoggIt.Log("Available height is lower than all systems heights");
+                }
+            }
+        }
+
+        private void GetPageContentWidth()
+        {
+            pageContentWidth = ViewModel.ViewModelLocator.Instance.Main.CurrentPageLayout.GetContentWidth();
+        }
+
+        private void GetPageContentHeight()
+        {
+            pageContentHeight = ViewModel.ViewModelLocator.Instance.Main.CurrentPageLayout.GetContentHeight();
+        }
+
+        private void GenerateSystemHeights()
+        {
+            if (systemHeights == null)
+            {
+                systemHeights = new Dictionary<int, double>();
+            }
+            if (systemHeights.Count != 0) //? maight be refactored to skip heights instead of creating collection from scratch
+            {
+                systemHeights.Clear();
+            }
+
+            foreach (var system in systemDimensionsInfo)
+            {
+                int index = systemDimensionsInfo.IndexOf(system);
+                systemHeights.Add(index, system.SystemHeight);
+            }
+        }
+        private void GenerateSystemDistances()
+        {
+            if (systemDistances == null)
+            {
+                systemDistances = new Dictionary<int, double>();
+            }
+            if (systemDistances.Count != 0)//? maight be refactored to skip heights instead of creating collection from scratch
+            {
+                systemDistances.Clear();
+            }
+
+            foreach (var system in systemDimensionsInfo)
+            {
+                int index = systemDimensionsInfo.IndexOf(system);
+                if (index == 0)
+                {
+                    systemDistances.Add(index, defaultTopSystemDistance);
+                }
+                else
+                {
+                    systemDistances.Add(index, defaultSystemDistance);
+                }
+            }
+        }
+        private void SetDefaultDistances()
+        {
+            var layout = ViewModel. ViewModelLocator.Instance.Main.CurrentLayout;
+            defaultSystemDistance = 2.5 * layout.PageProperties.StaffHeight.MMToTenths();
+            defaultTopSystemDistance = 3 * layout.PageProperties.StaffHeight.MMToTenths();
+            defaultStaffDistance = 1.7 * layout.PageProperties.StaffHeight.MMToTenths();
+        }
+    }
+}
---
+++ MusicXMLViewerWPF/LayoutControl/AdvancedLayoutHelpers/LayoutSystemInfo.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicXMLScore.LayoutControl
+{ 
+    class LayoutSystemInfo
+    {
+        private double systemHeight;
+        private double systemWidth;
+
+        //! distances between parts inside this system
+        private Dictionary<string, double> partStaffDistances;
+        //! calculated part heights 
+        private Dictionary<string, double> partHeights;
+        //! collection of measure widths to generate measures row
+        private Dictionary<string, double> measureSharedWidths;
+        //! collection of measures inside this system
+        private List<SharedMeasureProperties> measures;
+        //! measures X Coordinates inside system
+        private Dictionary<string, double> measureCoordsX;
+        //! measures Y Coordinates inside system
+        private Dictionary<string, double> measureCoordsY;
+        public double SystemHeight
+        {
+            get
+            {
+                return systemHeight;
+            }
+        }
+
+        public double SystemWidth
+        {
+            get
+            {
+                return systemWidth;
+            }
+        }
+
+        public LayoutSystemInfo(List<SharedMeasureProperties> measuresOfSystem)
+        {
+            measures = measuresOfSystem;
+        }
+        public void AddPartHeights(Dictionary<string, double> partHeights)
+        {
+            this.partHeights = partHeights;
+        }
+
+        public void AddPartDistances(Dictionary<string, double> partDistances)
+        {
+            partStaffDistances = partDistances;
+        }
+
+        public void CalculateSystemDimensions()
+        {
+            double heightParts = partHeights.Sum(x => x.Value);
+            double distances = partStaffDistances.Sum(x => x.Value);
+            systemHeight = heightParts + distances;
+            systemWidth = measureSharedWidths.Sum(x => x.Value);
+        }
+        private void GetSharedWidths()
+        {
+            measureSharedWidths = new Dictionary<string, double>();
+            foreach (var measure in measures)
+            {
+                measureSharedWidths.Add(measure.MeasureId, measure.SharedWidth);
+            }
+        }
+        private void GenerateMeasureXCoordinates()
+        {
+            double currentX = 0.0;
+            if (measureSharedWidths != null || measureSharedWidths.Count == 0)
+            {
+                Log.LoggIt.Log("no measures inside collection: X coordinates did not generated");
+            }
+            else
+            {
+                measureCoordsX = new Dictionary<string, double>();
+                foreach (var item in measureSharedWidths)
+                {
+                    measureCoordsX.Add(item.Key, currentX);
+                    currentX += item.Value;
+                }
+            }
+            if (currentX > systemWidth)
+            {
+                Log.LoggIt.Log($"System width too small!!! Now is: {systemWidth}, should be {currentX}");
+            }
+        }
+
+        public void UpdateSystemWidth(double width)
+        {
+            systemWidth = width;
+        }
+
+        public void UpdateSystemHeight(double height)
+        {
+            systemHeight = height;
+        }
+
+        private void GenerateMeasureYCoordinates()
+        {
+            double currentY = 0.0;
+            if (partStaffDistances != null || partStaffDistances.Count == 0)
+            {
+                Log.LoggIt.Log("no part staff distances specified inside collection: Y coordinates did not generated");
+            }
+            else
+            {
+                measureCoordsY = new Dictionary<string, double>();
+                foreach (var item in partStaffDistances)
+                {
+                    measureCoordsY.Add(item.Key, currentY);
+                    currentY += item.Value + partHeights[item.Key];
+                }
+            }
+            if (currentY > systemHeight)
+            {
+                Log.LoggIt.Log($"System height too small!!! Now is: {systemHeight}, should be {currentY}");
+            }
+        }
+        public void GenerateCoords()
+        {
+            GenerateMeasureXCoordinates();
+            GenerateMeasureYCoordinates();
+        }
+    }
+}
