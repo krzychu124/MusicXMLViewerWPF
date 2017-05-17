6a2f2b33895ea172239b6af011d7d0da3a86f91e
krzychu124 <krzychu124@gmail.com>
2017-05-17 22:02:24 +0200

    Added visuals count from Visual Container Host

--- MusicXMLViewerWPF/Helpers/DrawingVisualHost.cs
+++ MusicXMLViewerWPF/Helpers/DrawingVisualHost.cs
@@ -32,28 +32,43 @@ namespace MusicXMLScore.Helpers
                 return _containerVisual == null ? 0:1;
             }
         }
+        /// <summary>
+        /// Adds Visual to Visuals Container Host
+        /// </summary>
+        /// <param name="visual"></param>
         public void AddVisual(Visual visual)
         {
             _containerVisual.Children.Add(visual);
         }
 
         /// <summary>
-        /// Removes visual from Visual, VisualChild and LogicalChild collection
+        /// Removes visual from Visuals Container host
         /// </summary>
         /// <param name="visual"></param>
         public void DeleteVisual(Visual visual)
         {
-            visuals.Remove(visual);
-
-            RemoveVisualChild(visual);
+            if (visuals.Contains(visual))
+            {
+                _containerVisual.Children.Remove(visual);
+                visuals.Remove(visual);
+            }
+            else
+            {
+                Log.LoggIt.Log("Selected visual not found for removal", Log.LogType.Exception);
+            }
         }
 
         /// <summary>
-        /// Clears all Visual Collections
+        /// Clears all Visuals from Visual Container Host
         /// </summary>
         public void ClearVisuals()
         {
             _containerVisual.Children.Clear();
         }
+
+        /// <summary>
+        /// Count of visuals inside Visuals Container Host
+        /// </summary>
+        public int Count { get { return _containerVisual.Children.Count; } }
     }
 }
