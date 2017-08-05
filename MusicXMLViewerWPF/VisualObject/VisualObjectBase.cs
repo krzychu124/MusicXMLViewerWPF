c5f700dcb66fe252683e7c45db4219115bf36447
krzychu124 <krzychu124@gmail.com>
2017-08-05 04:49:40 +0200

    base visual object class with common properties

---
+++ MusicXMLViewerWPF/VisualObject/VisualObjectBase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using MusicXMLScore.Converters;
+using System.Runtime.CompilerServices;
+
+namespace MusicXMLScore.VisualObject
+{
+    class VisualObjectBase : INotifyPropertyChanged
+    {
+        private Brush _color;
+        private double _width;
+        private double _height;
+        public Canvas CanvasVisual { get; set; }
+        public Brush Color
+        {
+            get { return _color; }
+            set
+            {
+                Set(ref _color, value);
+            }
+        }
+
+        public double Width
+        {
+            get { return _width; }
+            set
+            {
+                Set(ref _width, value);
+            }
+        }
+
+        public double Height
+        {
+            get { return _height; }
+            set
+            {
+                Set(ref _height, value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName]string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
---
+++ MusicXMLViewerWPF/VisualObject/VisualObjectWithOffset.cs
@@ -0,0 +1,27 @@
+﻿using MusicXMLScore.Converters;
+
+namespace MusicXMLScore.VisualObject
+{
+    internal class VisualObjectWithOffset : VisualObjectBase
+    {
+        private double _horizontalOffset;
+        private double _verticalOffset;
+
+        public double HorizontalOffset
+        {
+            get { return _horizontalOffset; }
+            set
+            {
+                Set(ref _horizontalOffset, value);
+            }
+        }
+
+        public double VerticalOffset
+        {
+            get { return _verticalOffset; }
+            set {
+                Set(ref _verticalOffset, value);
+            }
+        }
+    }
+}
