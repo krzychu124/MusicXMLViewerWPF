c5162b19be13f24dfb1857bf5185c41cdc088a1d
krzychu124 <krzychu124@gmail.com>
2017-09-05 04:29:01 +0200

    added basic score builders, work-in-progress

---
+++ MusicXMLViewerWPF/Model/Builders/ScorePartwisePartBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicXMLScore.Model.Builders
+{
+    public class ScorePartwisePartBuilder
+    {
+        private readonly ScorePartwisePartMusicXML part;
+       
+
+        public ScorePartwisePartBuilder()
+        {
+            part = new ScorePartwisePartMusicXML
+            {
+                Measure = new List<ScorePartwisePartMeasureMusicXML>()
+            };
+        }
+
+        public ScorePartwisePartMusicXML Build()
+        {
+            if (part.Id == null)
+            {
+                part.Id = "P0";
+            }
+            return part;
+        }
+
+        public ScorePartwisePartBuilder AddMeasure(ScorePartwisePartMeasureMusicXML measure)
+        {
+            CheckMeasureId(measure);
+            part.Measure.Add(measure);
+            return this;
+        }
+
+        private void CheckMeasureId(ScorePartwisePartMeasureMusicXML measure)
+        {
+            if (measure.Number == null)
+            {
+                if (part.Measure.Count() == 0)
+                {
+                    measure.Number = 1 + "";
+                }else
+                if (int.TryParse(part.Measure.LastOrDefault().Number, out int number))
+                {
+                    // set next number
+                    measure.Number = number++ + "";
+                }
+                else
+                {
+                    Console.WriteLine("Part Builder measure number parse failed: " + measure.Number);
+                }
+            }
+        }
+    }
+}
---
+++ MusicXMLViewerWPF/Model/Builders/ScorePartwisePartMeasureBuilder.cs
@@ -0,0 +1,68 @@
+﻿using MusicXMLScore.Model.MeasureItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicXMLScore.Model.Builders
+{
+    public class ScorePartwisePartMeasureBuilder
+    {
+        private readonly ScorePartwisePartMeasureMusicXML measure;
+
+        public ScorePartwisePartMeasureBuilder()
+        {
+            measure = new ScorePartwisePartMeasureMusicXML
+            {
+                Items = new object[0]
+            };
+        }
+
+        public ScorePartwisePartMeasureBuilder AddBaseAttributes(AttributesMusicXML attributes)
+        {
+            measure.AppendNewItem(attributes);
+            return this;
+        }
+
+        public ScorePartwisePartMeasureBuilder AddNote(NoteMusicXML note)
+        {
+            measure.AppendNewItem(note);
+            return this;
+        }
+
+        public ScorePartwisePartMeasureBuilder AddRest(RestMusicXML rest)
+        {
+            measure.AppendNewItem(rest);
+            return this;
+        }
+
+        public ScorePartwisePartMeasureMusicXML Build()
+        {
+            CheckItemsOrder();
+            return measure;
+        }
+
+        //TODO add direction
+        //TODO set number (if none - auto generated)
+        //TODO set barline
+        //TODO add backup, forward
+        //TODO add print info (layout manager information - measure on new page/new line etc.)
+
+        private void CheckItemsOrder()
+        {
+            bool containsAttributes = measure.Items.Any(item => item.GetType() == typeof(AttributesMusicXML));
+            if (containsAttributes)
+            {
+                if (measure.Items[0].GetType() != typeof(AttributesMusicXML))
+                {
+                    var list = measure.Items.ToList();
+                    int attributeIndex = list.FindIndex(item => item.GetType() == typeof(AttributesMusicXML));
+                    var attributes = list.ElementAt(attributeIndex) as AttributesMusicXML;
+                    list.RemoveAt(attributeIndex);
+                    list.Insert(0, attributes);
+                }
+            }
+        }
+    }
+}
