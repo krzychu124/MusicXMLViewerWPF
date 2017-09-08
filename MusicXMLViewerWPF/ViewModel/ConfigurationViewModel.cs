c39f67620739adbfe74d95e713c7f9e3b9510daf
krzychu124 <krzychu124@gmail.com>
2017-09-08 02:02:28 +0200

    ok + cancel button - close window action added

--- MusicXMLViewerWPF/ViewModel/ConfigurationViewModel.cs
+++ MusicXMLViewerWPF/ViewModel/ConfigurationViewModel.cs
@@ -1,4 +1,7 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+using GalaSoft.MvvmLight;
+using MusicXMLScore.Helpers;
+using System.Windows;
 
 namespace MusicXMLScore.ViewModel
 {
@@ -7,5 +10,26 @@ namespace MusicXMLScore.ViewModel
     /// </summary>
     class ConfigurationViewModel : ViewModelBase
     {
+
+        public RelayCommand<Object> CancelCommand { get; set; }
+        public RelayCommand<Object> OKCommand { get; set; }
+
+        public ConfigurationViewModel()
+        {
+            OKCommand = new RelayCommand<object>(OnOkClick);
+            CancelCommand = new RelayCommand<Object>(OnCancelClick);
+        }
+
+        private void OnOkClick(object obj)
+        {
+            var w = obj as Window;
+            w.Close();
+        }
+
+        private void OnCancelClick(object window)
+        {
+            var w = window as Window;
+            w.Close();
+        }
     }
 }
