cec882c87f874e9c250ff9af4adbca33b121cc39
krzychu124 <krzychu124@gmail.com>
2017-09-11 02:26:49 +0200

    implmented base of abstract measure time

--- MusicXMLViewerWPF/ScoreLayout/MeasureLayouts/MeasureContent/AbstractClasses/AbstractTime.cs
+++ MusicXMLViewerWPF/ScoreLayout/MeasureLayouts/MeasureContent/AbstractClasses/AbstractTime.cs
@@ -1,25 +1,73 @@
-﻿using System;
+﻿using MusicXMLScore.Model.MeasureItems.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using MusicXMLScore.Helpers;
 
 namespace MusicXMLScore.ScoreLayout.MeasureLayouts.MeasureContent.AbstractClasses
 {
-    abstract class AbstractTime : IVisual
+    abstract class AbstractTime : IVisualHost
     {
-        private DrawingVisual visual;
-        public DrawingVisual GetVisual()
+        private string beatTime;
+        private string beatType;
+        private int[] compoundBeat;
+        private int[] compoundTime;
+        private readonly AbstractStaff staff;
+        private TimeSymbolMusicXML timeSymbol;
+        private readonly DrawingVisualHost visualHost;
+
+        protected AbstractTime(string beatTime, string beatType, AbstractStaff staff)
         {
-            return visual;
+            visualHost = new DrawingVisualHost();
+            SetTimeAndBeats(beatTime, beatType);
+            this.staff = staff;
+            this.timeSymbol = TimeSymbolMusicXML.normal;
+        }
+        protected AbstractTime(string beatTime, string beatType, TimeSymbolMusicXML timeSymbol, AbstractStaff staff)
+        {
+            visualHost = new DrawingVisualHost();
+            this.timeSymbol = timeSymbol;
+            SetTimeAndBeats(beatTime, beatType);
+            this.staff = staff;
+        }
+        public string BeatTime { get => beatTime; }
+        public string BeatType { get => beatType; }
+
+        public TimeSymbolMusicXML TimeSymbol { get => timeSymbol; set => timeSymbol = value; }
+
+        internal AbstractStaff Staff => staff;
+
+        public DrawingVisualHost GetVisualsContainer()
+        {
+            return visualHost;
         }
 
         public abstract double GetVisualWidth();
 
-        public void Update()
+        public void SetTimeAndBeats(string beatTime, string beatType)
         {
-            Console.WriteLine("Time Updated");
+            this.beatTime = beatTime;
+            this.beatType = beatType;
+            if (beatTime.Contains("+"))
+            {
+                var compound = beatTime.Split('+').ToList();
+                compoundTime = new int[compound.Count];
+                for (int i = 0; i < compound.Count; i++)
+                {
+                    compoundTime[i] = int.Parse(compound[i]);
+                }
+                compoundBeat = new int[] { int.Parse(beatType) };
+            }
+            else
+            {
+                compoundBeat = new int[] { int.Parse(beatType) };
+                compoundTime = new int[] { int.Parse(beatTime) };
+            }
         }
+
+        public abstract void Update();
     }
 }
