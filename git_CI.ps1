cd108b8dee8b686b08b44d65344cd33441aa14fd
Krzysztof Grabowski <krzysztof--grabowski@wp.pl>
2017-09-14 04:49:14 +0000

    Update git_CI.ps1

--- git_CI.ps1
+++ git_CI.ps1
@@ -1,1 +1,3 @@
-msbuild
+$msbuild = (Get-ItemProperty hklm:\software\Microsoft\MSBuild\ToolsVersions\4.0).MSBuildToolsPath
+$MyDir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition) 
+. "$msbuild\msbuild.exe"
