d58bfa8d517cccd8a7f4546f84343d50794a8aaa
Krzysztof Grabowski <krzysztof--grabowski@wp.pl>
2017-09-15 01:22:36 +0000

    Update git_CI.ps1

--- git_CI.ps1
+++ git_CI.ps1
@@ -2,5 +2,5 @@
 $collectionOfArgs = "MusicXMLViewerWPF.sln"
 #$msB = Resolve-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\*\*\MSBuild\*\bin\MSBuild.exe"
 $test = '"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe"'
-C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe &collectionOfArgs
+& "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\MSBuild.exe" ./MusicXMLViewerWPF.sln
 #& $test $collectionOfArgs
