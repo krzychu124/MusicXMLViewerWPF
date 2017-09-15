b73841a62d99771260a5e3d92e0596308a792e04
krzychu124 <krzychu124@gmail.com>
2017-09-15 03:12:29 +0200

    update

--- git_CI.ps1
+++ git_CI.ps1
@@ -1,25 +1,4 @@
 # $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-function Resolve-MsBuild {
-	$msb2017 = Resolve-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\*\*\MSBuild\*\bin\MSBuild.exe" -ErrorAction SilentlyContinue
-	if($msb2017) {
-		Write-Host "Found MSBuild 2017 (or later)."
-		Write-Host $msb2017
-		return $msb2017
-	}
-
-	$msBuild2015 = "${env:ProgramFiles(x86)}\MSBuild\14.0\bin\msbuild.exe"
-
-	if(-not (Test-Path $msBuild2015)) {
-		throw 'Could not find MSBuild 2015 or later.'
-	}
-
-	Write-Host "Found MSBuild 2015."
-	Write-Host $msBuild2015
-
-	return $msBuild2015
-}
-
-$msBuild = Resolve-MsBuild
-
-$collectionOfArgs = @("MusicXMLViewerWPF.sln")
-& $msbuild .\MusicXMLViewerWPF.sln
+$collectionOfArgs = "MusicXMLViewerWPF.sln"
+$msB = Resolve-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\*\*\MSBuild\*\bin\MSBuild.exe"
+& $msB[0] $collectionOfArgs
