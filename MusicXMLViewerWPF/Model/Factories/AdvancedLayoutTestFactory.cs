ebdff632b7e6f9a5977b8ec4bbc5870b1ff9bca1
krzychu124 <krzychu124@gmail.com>
2017-09-17 03:38:26 +0200

    factory stub for advanced layout test page WiP

---
+++ MusicXMLViewerWPF/Model/Factories/AdvancedLayoutTestFactory.cs
@@ -0,0 +1,18 @@
+﻿using MusicXMLScore.ScoreLayout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicXMLScore.Model.Factories
+{
+    class AdvancedLayoutTestFactory
+    {
+        public static AbstractScorePage GetScorePage()
+        {
+
+            return null;
+        }
+    }
+}
