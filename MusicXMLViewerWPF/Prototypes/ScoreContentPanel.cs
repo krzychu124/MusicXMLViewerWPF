0db251511f858c22d8182798585f0df0d123c626
krzychu124 <krzychu124@gmail.com>
2017-10-01 21:14:22 +0200

    advanced custom layout panel, if child doesn't fit in panel space and NextPanel property set, it will be moved to them

--- MusicXMLViewerWPF/Prototypes/ScoreContentPanel.cs
+++ MusicXMLViewerWPF/Prototypes/ScoreContentPanel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicXMLScore.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,47 +11,41 @@ namespace MusicXMLScore.Prototypes
 {
     class ScoreContentPanel : Panel
     {
-        private RowTopMargins margins;
-        private readonly List<UIElement> childsRow = new List<UIElement>();
-
-        public ScoreContentPanel()
-        {
-            margins = new RowTopMargins(this);
-        }
-
-
-
-        public static bool GetBottom(DependencyObject obj)
-        {
-            return (bool)obj.GetValue(BottomProperty);
-        }
-
-        public static void SetBottom(DependencyObject obj, bool value)
-        {
-            obj.SetValue(BottomProperty, value);
-        }
 
         // Using a DependencyProperty as the backing store for Bottom.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty BottomProperty =
             DependencyProperty.RegisterAttached("Bottom", typeof(bool), typeof(ScoreContentPanel), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsParentArrange));
 
-
-
-        public RowTopMargins RowTopMargins { get => margins; set => margins = value; }
-
-        public static bool GetFullWidthItem(DependencyObject obj)
-        {
-            return (bool)obj.GetValue(FullWidthItemProperty);
-        }
-
-        public static void SetFullWidthItem(DependencyObject obj, bool value)
-        {
-            obj.SetValue(FullWidthItemProperty, value);
-        }
-
         // Using a DependencyProperty as the backing store for FullWidthItem.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty FullWidthItemProperty =
             DependencyProperty.RegisterAttached("FullWidthItem", typeof(bool), typeof(ScoreContentPanel), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsParentArrange));
+
+        // Using a DependencyProperty as the backing store for NextPanel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NextPanelProperty =
+            DependencyProperty.Register(nameof(NextPanel), typeof(ScoreContentPanel), typeof(ScoreContentPanel), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsParentMeasure));
+
+        // Using a DependencyProperty as the backing store for StretchLastRow.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StretchLastRowProperty =
+            DependencyProperty.Register(nameof(StretchLastRow), typeof(bool), typeof(ScoreContentPanel), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsArrange));
+        private readonly List<UIElement> childsRow = new List<UIElement>();
+        private RowTopMargins margins;
+
+        public ScoreContentPanel()
+        {
+            margins = new RowTopMargins(this);
+
+        }
+
+
+        public ScoreContentPanel NextPanel
+        {
+            get { return (ScoreContentPanel)GetValue(NextPanelProperty); }
+            set { SetValue(NextPanelProperty, value); }
+        }
+
+
+
+        public RowTopMargins RowTopMargins { get => margins; set => margins = value; }
 
 
 
@@ -60,21 +55,34 @@ namespace MusicXMLScore.Prototypes
             set { SetValue(StretchLastRowProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for StretchLastRow.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty StretchLastRowProperty =
-            DependencyProperty.Register("StretchLastRow", typeof(bool), typeof(ScoreContentPanel), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsArrange));
 
-       
 
-        protected override Size MeasureOverride(Size availableSize)
+        public static bool GetBottom(DependencyObject obj)
         {
-            Size size = availableSize;
+            return (bool)obj.GetValue(BottomProperty);
+        }
 
-            foreach (UIElement child in InternalChildren)
-            {
-                child.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
-            }
-            return base.MeasureOverride(availableSize);
+        public static bool GetFullWidthItem(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(FullWidthItemProperty);
+        }
+
+        public static void SetBottom(DependencyObject obj, bool value)
+        {
+            obj.SetValue(BottomProperty, value);
+        }
+
+        public static void SetFullWidthItem(DependencyObject obj, bool value)
+        {
+            obj.SetValue(FullWidthItemProperty, value);
+        }
+
+        public bool HasNextPanel() => NextPanel != null;
+
+        public void InsertChild(UIElement child)
+        {
+            this.Children.Insert(0, child);
+            Console.WriteLine("Child inserted");
         }
 
         protected override Size ArrangeOverride(Size finalSize)
@@ -85,15 +93,19 @@ namespace MusicXMLScore.Prototypes
             int currentRowIndex = 0;
             foreach (UIElement child in InternalChildren)
             {
-
+                //var measure = child as MeasurePrototype;
+                //if(measure != null)
+                //{
+                //    Console.WriteLine("A_" + measure.Number + "_" + measure.Color);
+                //}
                 var fullWidthItem = child.GetValue(FullWidthItemProperty);
                 var bottomPlaced = child.GetValue(BottomProperty);
-                if(bottomPlaced != null &(bool)bottomPlaced)
+                if ((bool)bottomPlaced)
                 {
-                    child.Arrange(new Rect(0,finalSize.Height - child.DesiredSize.Height, finalSize.Width, child.DesiredSize.Height));
+                    child.Arrange(new Rect(0, finalSize.Height - child.DesiredSize.Height, finalSize.Width, child.DesiredSize.Height));
                     continue;
                 }
-                if (fullWidthItem != null && (bool)fullWidthItem)
+                if ((bool)fullWidthItem)
                 {
                     if (childsRow.Count != 0)
                     {
@@ -108,14 +120,14 @@ namespace MusicXMLScore.Prototypes
                 }
                 double width = child.DesiredSize.Width;
                 double height = child.DesiredSize.Height;
-                if ((bool)bottomPlaced)
-                {
-
-                }
                 if (cursorX + width > finalSize.Width)
                 {
-                    var rowHeight = childsRow.Max(i => i.DesiredSize.Height);
-                    RearrangeRow(childsRow, finalSize.Width, cursorY);
+                    double rowHeight = child.DesiredSize.Height;
+                    if (childsRow.Count != 0)
+                    {
+                        rowHeight = childsRow.Max(i => i.DesiredSize.Height);
+                        RearrangeRow(childsRow, finalSize.Width, cursorY);
+                    }
                     cursorX = 0;
                     cursorY += rowHeight + GetMargin(++currentRowIndex);
                 }
@@ -127,7 +139,92 @@ namespace MusicXMLScore.Prototypes
             {
                 RearrangeRow(childsRow, finalSize.Width, cursorY);
             }
+            Console.WriteLine("Arranged");
             return base.ArrangeOverride(finalSize);
+        }
+
+
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            Size size = base.MeasureOverride(availableSize);
+            bool hasNextPanel = HasNextPanel();
+            double cursorX = 0;
+            double cursorY = GetMargin(0);
+            int currentRowIndex = 0;
+            double currentRowHeight = 0;
+            foreach (UIElement child in InternalChildren)
+            {
+                //var measure = child as MeasurePrototype;
+                //if (measure != null)
+                //{
+                //    Console.WriteLine("M_" + measure.Number +"_"+measure.Color);
+                //}
+                var fullWidthItem = child.GetValue(FullWidthItemProperty);
+                child.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                currentRowHeight = child.DesiredSize.Height > currentRowHeight ? child.DesiredSize.Height : currentRowHeight;
+                if ((bool)fullWidthItem)
+                {
+                    double calculatedHeight = cursorY + currentRowHeight + GetMargin(++currentRowIndex);
+                    if (calculatedHeight > availableSize.Height)
+                    {
+                        if (HasNextPanel())
+                        {
+                            var sourceArray = new UIElement[InternalChildren.Count];
+                            InternalChildren.CopyTo(sourceArray, 0);
+                            int index = InternalChildren.IndexOf(child);
+                            var array = new UIElement[InternalChildren.Count - index];
+                            Array.Copy(sourceArray, index, array, 0, InternalChildren.Count - index);
+                            Array.Reverse(array);
+                            foreach (var item in array)
+                            {
+                                InternalChildren.Remove(item);
+                                NextPanel.InsertChild(item);
+                            }
+                            return size;
+                        }
+                    }
+                    cursorY += currentRowHeight + GetMargin(++currentRowIndex);
+                    cursorX = 0;
+                }
+                else
+                {
+                    if (cursorX + child.DesiredSize.Width > availableSize.Width)
+                    {
+                        double calculatedHeight = cursorY + currentRowHeight + GetMargin(++currentRowIndex);
+                        if (calculatedHeight + child.DesiredSize.Height > availableSize.Height)
+                        {
+                            if (HasNextPanel())
+                            {
+                                var sourceArray = new UIElement[InternalChildren.Count];
+                                InternalChildren.CopyTo(sourceArray, 0);
+                                int index = InternalChildren.IndexOf(child);
+                                var array = new UIElement[InternalChildren.Count - index];
+                                Array.Copy(sourceArray, index, array, 0, InternalChildren.Count - index);
+                                Array.Reverse(array);
+                                foreach (var item in array)
+                                {
+                                    InternalChildren.Remove(item);
+                                    NextPanel.InsertChild(item);
+                                }
+                                return size;
+                            }
+                        }
+                        cursorX = child.DesiredSize.Width;
+                        cursorY = calculatedHeight;
+                    }
+                    else
+                    {
+                        cursorX += child.DesiredSize.Width;
+                    }
+                }
+            }
+            Console.WriteLine("Measured");
+            return size;
+        }
+        private double GetMargin(int index)
+        {
+            return margins.Count > index ? margins[index].TopMargin : 0.0;
         }
 
         private void RearrangeRow(List<UIElement> childsRow, double width, double cursorY)
@@ -142,10 +239,6 @@ namespace MusicXMLScore.Prototypes
                 cursorX += newWidth;
             }
             childsRow.Clear();
-        }
-        private double GetMargin(int index)
-        {
-            return margins.Count > index ? margins[index].TopMargin : 0.0;
         }
     }
 }
