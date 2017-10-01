b806ceeaa49a7dc3494bde307cb04d49faf4170a
krzychu124 <krzychu124@gmail.com>
2017-10-01 21:11:49 +0200

    advanced layout calculations prototype view

--- MusicXMLViewerWPF/ViewModel/AdvancedPageViewModel.cs
+++ MusicXMLViewerWPF/ViewModel/AdvancedPageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using GalaSoft.MvvmLight;
+using MusicXMLScore.Helpers;
+using MusicXMLScore.Prototypes;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,27 +14,87 @@ namespace MusicXMLScore.ViewModel
 {
     class AdvancedPageViewModel : ViewModelBase
     {
-        public Thickness Margin { get; set; }
-        public ObservableCollection<UIElement> PageContent { get; set; }
+        private double pageHeight;
+        private double pageWidth;
 
-        public AdvancedPageViewModel()
+        public double PageWidth { get { return pageWidth; } set { Set(nameof(PageWidth), ref pageWidth, value); } }
+        public double PageHeight { get { return pageHeight; } set { Set(nameof(PageHeight), ref pageHeight, value); } }
+        public Thickness Margin { get; set; }
+
+        public ContextMenu ContextMenu { get; set; }
+        public RelayCommand TestCommand { get; set; }
+        public ScoreContentPanel CurrentPanel { get; set; }
+        public ScoreContentPanel NextPanel { get; set; }
+
+        private readonly Random random = new Random();
+
+        public AdvancedPageViewModel(string test)
         {
-            Margin = new Thickness(20, 20, 20, 30);
-            PageContent = new ObservableCollection<UIElement>();
-            var block = new TextBlock
+            TestCommand = new RelayCommand(OnTestCommand);
+
+            var cMenu = new ContextMenu();
+            var menuItem = new MenuItem() { Header = "Add measure", Command = new RelayCommand(OnAddMeasure) };
+            cMenu.Items.Add(menuItem);
+            ContextMenu = cMenu;
+            var childs = Model.Factories.AdvancedLayoutTestFactory.GetLayout();
+            if (test.Equals("empty"))
             {
-                Text = "test",
-                Width = 50
-            };
-            DockPanel.SetDock(block, Dock.Bottom);
-            var block2 = new TextBlock
+                childs = new List<UIElement>();
+            }
+            CurrentPanel = new ScoreContentPanel()
             {
-                Text = "test2"
+                Width = 1200,
+                Height = 1000,
+                StretchLastRow = true
             };
-            DockPanel.SetDock(block2, Dock.Right);
-            DockPanel.SetDock(block2, Dock.Top);
-            PageContent.Add(block);
-            PageContent.Add(block2);
+            PageWidth = CurrentPanel.Width;
+            PageHeight = CurrentPanel.Height;
+            SetDefaultRows();
+            foreach (MeasurePrototype item in childs)
+            {
+                CurrentPanel.Children.Add(item);
+                item.Number = CurrentPanel.Children.IndexOf(item as UIElement);
+            }
+
         }
+
+        private void OnTestCommand()
+        {
+            if (PageHeight > 300)
+            {
+                CurrentPanel.Height = CurrentPanel.Height - 30;
+                PageHeight = CurrentPanel.Height;
+
+                CurrentPanel.Width = CurrentPanel.Width - 30;
+                PageWidth = CurrentPanel.Width;
+            }
+        }
+
+        private void OnAddMeasure()
+        {
+            if (CurrentPanel != null)
+            {
+                CurrentPanel.Children.Add(new MeasurePrototype { MinWidth = random.Next(150) + 200 });
+            }
+            Console.WriteLine("added measure");
+        }
+
+
+        private void SetDefaultRows()
+        {
+            var rowCollection = new RowTopMargins(CurrentPanel)
+            {
+                new RowTopMargin { TopMargin = 40 },
+                new RowTopMargin { TopMargin = 40 },
+                new RowTopMargin { TopMargin = 40 },
+                new RowTopMargin { TopMargin = 40 },
+                new RowTopMargin { TopMargin = 40 },
+                new RowTopMargin { TopMargin = 40 },
+                new RowTopMargin { TopMargin = 40 },
+                new RowTopMargin { TopMargin = 40 }
+            };
+            CurrentPanel.RowTopMargins = rowCollection;
+        }
+
     }
 }
