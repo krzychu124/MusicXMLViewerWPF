9fb72d3eff111b6a2388feb9094d830e35c9ecc1
krzychu124 <krzychu124@gmail.com>
2017-10-01 21:23:53 +0200

    readability improvements

--- MusicXMLViewerWPF/ViewModel/PageViewModel.cs
+++ MusicXMLViewerWPF/ViewModel/PageViewModel.cs
@@ -9,6 +9,8 @@ using MusicXMLScore.DrawingHelpers;
 using MusicXMLScore.Helpers;
 using MusicXMLViewerWPF;
 using System.ComponentModel;
+using MusicXMLScore.Prototypes;
+using System.Collections.Generic;
 
 namespace MusicXMLScore.ViewModel
 {
@@ -17,23 +19,15 @@ namespace MusicXMLScore.ViewModel
     /// </summary>
     class PageViewModel : ViewModelBase
     {
-        #region Private Fiels
-        private ObservableCollection<UIElement> pageCanvas = new ObservableCollection<UIElement>();
-        private DrawingVisualHost page = new DrawingVisualHost();
-        private double pageHeight;
-        private double pageWidth;
-        public AdvancedPageViewModel AdvancedPageViewContent { get; set; }
-
-        public string PageNumber { get; set; }
-        public TextAlignment PageNumberAlignment { get; set; }
-        private ScoreLayout.AbstractScorePage scorePage;
-        #endregion
-        #region NewConcept
         PageDrawingSystem newPage;
+        private DrawingVisualHost page = new DrawingVisualHost();
+        private ObservableCollection<UIElement> pageCanvas = new ObservableCollection<UIElement>();
+        private double pageHeight;
         int pageIndex;
-        #endregion
+        private double pageWidth;
+        Random random = new Random();
+        private ScoreLayout.AbstractScorePage scorePage;
 
-        #region Contructors
         public PageViewModel()
         {
             TestCommand = new RelayCommand(OnTestCommand);
@@ -45,11 +39,11 @@ namespace MusicXMLScore.ViewModel
             AddPartSegment();
         }
 
-        public PageViewModel(int pageIndex):this()
+        public PageViewModel(int pageIndex) : this()
         {
             this.pageIndex = pageIndex;
         }
-        //! test
+
         public PageViewModel(Canvas page)
         {
             Point dimensions = ViewModelLocator.Instance.Main.CurrentPageLayout.PageDimensions.GetPageDimensionsInPx();
@@ -61,7 +55,7 @@ namespace MusicXMLScore.ViewModel
 
         public PageViewModel(ScoreLayout.AbstractScorePage page)
         {
-            AdvancedPageViewContent = new AdvancedPageViewModel();
+            AdvancedPageViewContent = new AdvancedPageViewModel("asd");
             TestCommand = new RelayCommand(OnTestCommand);
             PageWidth = page.Width;
             PageHeight = page.Height;
@@ -79,6 +73,26 @@ namespace MusicXMLScore.ViewModel
             PageCanvas.Add(canvas);
         }
 
+        public PageViewModel(ScorePartwiseMusicXML scorePartwise, int index)
+        {
+            pageIndex = index;
+            newPage = new PageDrawingSystem(scorePartwise, pageIndex);
+            Point dimensions = ViewModelLocator.Instance.Main.CurrentPageLayout.PageDimensions.GetPageDimensionsInPx();
+            PageWidth = dimensions.X;
+            PageHeight = dimensions.Y;
+            PageCanvas.Add(newPage.PageCanvas);
+        }
+
+        public AdvancedPageViewModel AdvancedPageViewContent { get; set; }
+        public ContextMenu ContextMenu { get; set; }
+        public DrawingVisualHost Page { get { return page; } set { Set(nameof(Page), ref page, value); } }
+        public ObservableCollection<UIElement> PageCanvas { get { return pageCanvas; } set { pageCanvas = value; } }
+        public double PageHeight { get { return pageHeight; } set { Set(nameof(PageHeight), ref pageHeight, value); } }
+        public string PageNumber { get; set; } = "0";
+        public TextAlignment PageNumberAlignment { get; set; } = TextAlignment.Left;
+        public double PageWidth { get { return pageWidth; } set { Set(nameof(PageWidth), ref pageWidth, value); } }
+        public RelayCommand TestCommand { get; set; }
+
         public void ScorePagePropertyChanged(object o, PropertyChangedEventArgs a)
         {
             switch (a.PropertyName)
@@ -94,44 +108,23 @@ namespace MusicXMLScore.ViewModel
             }
         }
 
-        public PageViewModel(ScorePartwiseMusicXML scorePartwise, int index)
-        {
-            pageIndex = index;
-            newPage = new PageDrawingSystem(scorePartwise, pageIndex);
-            Point dimensions = ViewModelLocator.Instance.Main.CurrentPageLayout.PageDimensions.GetPageDimensionsInPx();
-            PageWidth = dimensions.X;
-            PageHeight = dimensions.Y;
-            PageCanvas.Add(newPage.PageCanvas);
-        }
-        #endregion
-
-        #region Properties
-        public ObservableCollection<UIElement> PageCanvas { get { return pageCanvas; } set { pageCanvas = value; } }
-        public DrawingVisualHost Page { get { return page; } set { Set(nameof(Page), ref page, value); } }
-        public RelayCommand TestCommand { get; set; }
-        public double PageHeight { get { return pageHeight; } set { Set(nameof(PageHeight), ref pageHeight, value); } }
-        public double PageWidth { get { return pageWidth; } set { Set(nameof(PageWidth), ref pageWidth, value); } }
-        #endregion
-
-        #region Methods
         private void AddPartSegment()
         {
             PageCanvas.Add(page);
         }
 
-        #endregion
-
-        #region Commands, Action<>, Func<>
         private void OnTestCommand()
         {
             if (PageHeight > 300)
             {
-                scorePage.Height = scorePage.Height - 30;
-                PageHeight = scorePage.Height;
-                scorePage.Width = scorePage.Width - 30;
-                PageWidth = scorePage.Width;
+                if (scorePage != null)
+                {
+                    scorePage.Height = scorePage.Height - 30;
+                    PageHeight = scorePage.Height;
+                    scorePage.Width = scorePage.Width - 30;
+                    PageWidth = scorePage.Width;
+                }
             }
         }
-        #endregion
     }
 }
