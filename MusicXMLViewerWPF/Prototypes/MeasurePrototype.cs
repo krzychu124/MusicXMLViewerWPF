75242be99d44a875246584a1fc83ef963c9035af
krzychu124 <krzychu124@gmail.com>
2017-10-01 21:10:06 +0200

    added color background, dimensions info drawn on background

--- MusicXMLViewerWPF/Prototypes/MeasurePrototype.cs
+++ MusicXMLViewerWPF/Prototypes/MeasurePrototype.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicXMLScore.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,8 @@ namespace MusicXMLScore.Prototypes
     class MeasurePrototype : FrameworkElement
     {
         DrawingVisual visual;
-        Brush color;
+        private Brush color;
+        public int Number { get; set; }
         public MeasurePrototype()
         {
             visual = new DrawingVisual();
@@ -21,9 +23,11 @@ namespace MusicXMLScore.Prototypes
             AddVisualChild(visual);
             AddLogicalChild(visual);
             SizeChanged += MeasurePrototype_SizeChanged;
-           
+
         }
         protected override int VisualChildrenCount => 1;
+
+        public String Color { get => color.ToString();  }
 
         protected override Visual GetVisualChild(int index)
         {
@@ -32,21 +36,31 @@ namespace MusicXMLScore.Prototypes
 
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
-           Redraw(sizeInfo.NewSize);
+            Redraw(sizeInfo.NewSize);
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);//test 
         }
 
         private void MeasurePrototype_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            Console.WriteLine(e.PreviousSize + ", new: "+e.NewSize);
+            Console.WriteLine(e.PreviousSize + ", new: " + e.NewSize);
             Redraw(e.NewSize);
         }
 
         private void Redraw(Size newSize)
         {
-            visual.Children.Clear();
+           // visual.Children.Clear(); //test
+            //Console.WriteLine("Was: " + color + " " + Number);
             using (DrawingContext dc = visual.RenderOpen())
             {
                 dc.DrawRectangle(color, new Pen(), new Rect(0, 0, newSize.Width, newSize.Height));
+                //debug
+                //Console.WriteLine("Is: " + color + " " + Number);
+                Helpers.DrawingHelpers.DrawString(dc, Number.ToString() + " min width: " + MinWidth.ToString(), TypeFaces.GetTextFont(), Brushes.Black, 5, 10, 10);
+                Helpers.DrawingHelpers.DrawString(dc, "actual width: " + ActualWidth.ToString("#.0"), TypeFaces.GetTextFont(), Brushes.Black, 5, 22, 10);
             }
         }
     }
