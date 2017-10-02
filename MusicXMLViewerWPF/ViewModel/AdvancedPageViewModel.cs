512e7d51b3871611c27381edc57934cac9535f62
krzychu124 <krzychu124@gmail.com>
2017-10-02 02:16:56 +0200

    improved measure visual rendering, added option to insert each measure on click added option to add remaining measures added option to refresh layout of page

--- MusicXMLViewerWPF/ViewModel/AdvancedPageViewModel.cs
+++ MusicXMLViewerWPF/ViewModel/AdvancedPageViewModel.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using MusicXMLScore.LayoutControl;
 
 namespace MusicXMLScore.ViewModel
 {
@@ -17,16 +18,24 @@ namespace MusicXMLScore.ViewModel
         private double pageHeight;
         private double pageWidth;
 
-        public double PageWidth { get { return pageWidth; } set { Set(nameof(PageWidth), ref pageWidth, value); } }
-        public double PageHeight { get { return pageHeight; } set { Set(nameof(PageHeight), ref pageHeight, value); } }
-        public Thickness Margin { get; set; }
-
-        public ContextMenu ContextMenu { get; set; }
-        public RelayCommand TestCommand { get; set; }
-        public ScoreContentPanel CurrentPanel { get; set; }
-        public ScoreContentPanel NextPanel { get; set; }
-
         private readonly Random random = new Random();
+
+
+        public AdvancedPageViewModel(int number)
+        {
+            TestCommand = new RelayCommand(OnTestCommand);
+            RefreshPageCommand = new RelayCommand(OnRefreshPageCommand);
+            CurrentPanel = new ScoreContentPanel()
+            {
+                Width = 1200,
+                Height = 1000,
+                StretchLastRow = true
+            };
+
+            PageWidth = CurrentPanel.Width;
+            PageHeight = CurrentPanel.Height;
+            SetDefaultRows();
+        }
 
         public AdvancedPageViewModel(string test)
         {
@@ -50,12 +59,52 @@ namespace MusicXMLScore.ViewModel
             PageWidth = CurrentPanel.Width;
             PageHeight = CurrentPanel.Height;
             SetDefaultRows();
-            foreach (MeasurePrototype item in childs)
+            foreach (MeasureVisualPrototype item in childs)
             {
                 CurrentPanel.Children.Add(item);
-                item.Number = CurrentPanel.Children.IndexOf(item as UIElement);
+                //item.Number = CurrentPanel.Children.IndexOf(item as UIElement);
             }
 
+        }
+
+        public ContextMenu ContextMenu { get; set; }
+        public ScoreContentPanel CurrentPanel { get; set; }
+        public Thickness Margin { get; set; }
+        public ScoreContentPanel NextPanel { get; set; }
+        public double PageHeight { get { return pageHeight; } set { Set(nameof(PageHeight), ref pageHeight, value); } }
+
+        public double PageWidth { get { return pageWidth; } set { Set(nameof(PageWidth), ref pageWidth, value); } }
+        public RelayCommand RefreshPageCommand { get; set; }
+        public RelayCommand TestCommand { get; set; }
+        internal void AddAllNextMeasures(List<MeasureSegmentController> list)
+        {
+            foreach (var measure in list)
+            {
+                AddNextMeasure(measure);
+            }
+        }
+
+        internal void AddNextMeasure(MeasureSegmentController measureSegmentController)
+        {
+            CurrentPanel.Children.Add(new MeasureVisualTest(measureSegmentController));
+            Console.WriteLine("added measure: " + measureSegmentController.MeasureId);
+        }
+
+        private void OnAddMeasure()
+        {
+            if (CurrentPanel != null)
+            {
+                CurrentPanel.Children.Add(new MeasureVisualPrototype { MinWidth = random.Next(300) + 100 });
+            }
+            Console.WriteLine("added measure");
+        }
+
+        private void OnRefreshPageCommand()
+        {
+            if (CurrentPanel != null)
+            {
+                CurrentPanel.InvalidateMeasure();
+            }
         }
 
         private void OnTestCommand()
@@ -70,31 +119,30 @@ namespace MusicXMLScore.ViewModel
             }
         }
 
-        private void OnAddMeasure()
-        {
-            if (CurrentPanel != null)
-            {
-                CurrentPanel.Children.Add(new MeasurePrototype { MinWidth = random.Next(150) + 200 });
-            }
-            Console.WriteLine("added measure");
-        }
-
 
         private void SetDefaultRows()
         {
+            //TODO dynamic margin setting 
             var rowCollection = new RowTopMargins(CurrentPanel)
             {
-                new RowTopMargin { TopMargin = 40 },
-                new RowTopMargin { TopMargin = 40 },
-                new RowTopMargin { TopMargin = 40 },
-                new RowTopMargin { TopMargin = 40 },
-                new RowTopMargin { TopMargin = 40 },
-                new RowTopMargin { TopMargin = 40 },
-                new RowTopMargin { TopMargin = 40 },
-                new RowTopMargin { TopMargin = 40 }
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 },
+                new RowTopMargin { TopMargin = 100 }
             };
             CurrentPanel.RowTopMargins = rowCollection;
         }
-
     }
 }
